e095fcd85755b81541dd13c7d80d8426c0e6ab19
João Daher <joao@daher.dev>
2021-02-03 08:46:19 -0300

    Monkey patch drf-extensions
    Because pypi does not accept git dependencies

--- drf_kit/__init__.py
+++ drf_kit/__init__.py
@@ -1,3 +1,10 @@
+# Monkey patch until : https://github.com/chibisov/drf-extensions/issues/294
+from django.db.models.sql import datastructures
+from django.core.exceptions import EmptyResultSet
+
+datastructures.EmptyResultSet = EmptyResultSet
+
+
 # because we can't trust None, that could mean
 # "not set yet" or "set as None:
 UNSET = object()
