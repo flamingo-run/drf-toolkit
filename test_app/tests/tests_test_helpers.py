00515e913744638daf03277aea715a56421d3e65
João Daher <joao@daher.dev>
2021-07-30 15:47:33 -0300

    test: add test to cache-lock assertion helper

--- test_app/tests/tests_test_helpers.py
+++ test_app/tests/tests_test_helpers.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 
 from drf_kit.tests import BaseApiTest
+from test_app import tasks
 
 
 class TestPendingMigrations(BaseApiTest):
@@ -30,3 +31,67 @@ class TestPendingMigrations(BaseApiTest):
                 self.assertNoPendingMigration("test_app")
 
             mocked.assert_called_once()
+
+
+class TestLockAssertion(BaseApiTest):
+    def test_assert_locked_with_parameters(self):
+        with self.patch_cache_lock() as lock:
+            tasks.LockableTask().run()
+
+        lock.assert_called_with("triwizard", timeout=42)
+
+    def test_assert_locked_with_wrong_args(self):
+        with self.patch_cache_lock() as lock:
+            tasks.LockableTask().run()
+
+        expected_message = "Expected to lock with quidditch, but locked with triwizard"
+        with self.assertRaisesMessage(AssertionError, expected_message=expected_message):
+            lock.assert_called_with("quidditch", timeout=42)
+
+    def test_assert_locked_with_wrong_kwargs(self):
+        with self.patch_cache_lock() as lock:
+            tasks.LockableTask().run()
+
+        expected_message = "Expected to lock with timeout=666, but locked with 42"
+        with self.assertRaisesMessage(AssertionError, expected_message=expected_message):
+            lock.assert_called_with("triwizard", timeout=666)
+
+    def test_assert_locked_with_wrong_args_amount(self):
+        with self.patch_cache_lock() as lock:
+            tasks.LockableTask().run()
+
+        expected_message = "Expected to lock with 2 args: triwizard, inconvenient. But locked with 1 args: triwizard"
+        with self.assertRaisesMessage(AssertionError, expected_message=expected_message):
+            lock.assert_called_with("triwizard", "inconvenient", timeout=42)
+
+    def test_assert_locked_with_wrong_kwargs_amount(self):
+        with self.patch_cache_lock() as lock:
+            tasks.LockableTask().run()
+
+        expected_message = "Expected to lock with 2 kwargs: timeout, inconvenient. But locked with 1 kwargs: timeout"
+        with self.assertRaisesMessage(AssertionError, expected_message=expected_message):
+            lock.assert_called_with("triwizard", timeout=42, inconvenient=55)
+
+    def test_assert_locked_when_locked(self):
+        with self.patch_cache_lock() as lock:
+            tasks.LockableTask().run()
+
+        lock.assert_called()
+
+    def test_assert_locked_when_not_locked(self):
+        with self.patch_cache_lock() as lock:
+            tasks.NotifyMinisterOfMagicTask().run()
+
+        expected_message = "Expected to be called, but it was not"
+        with self.assertRaisesMessage(AssertionError, expected_message=expected_message):
+            lock.assert_called_with()
+
+    def test_execute_side_effect(self):
+        def _effect():
+            raise Exception("Dementors")
+
+        with self.patch_cache_lock(side_effect=_effect) as lock:
+            with self.assertRaisesMessage(Exception, expected_message="Dementors"):
+                tasks.LockableTask().run()
+
+        lock.assert_called()
