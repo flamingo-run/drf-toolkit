4e2c27863a3269610dcf163dd62dfdd7c638fb6a
João Daher <joao@daher.dev>
2021-07-30 15:47:33 -0300

    test: add test to cache-lock assertion helper

--- test_app/tasks.py
+++ test_app/tasks.py
@@ -1,3 +1,12 @@
+from django.core.cache import cache
+
+
 class NotifyMinisterOfMagicTask:
     def run(self, *args, **kwargs):
         print("Points to Griffindor!")
+
+
+class LockableTask:
+    def run(self):
+        with cache.lock("triwizard", timeout=42):
+            print("The goblet of fire is mine.")
