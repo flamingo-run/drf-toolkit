819161300379f88a8859893a962da0ff3646b743
João Daher <joao@daher.dev>
2022-01-18 13:51:44 -0300

    feat: add combinations for base models (ordered + inheritance + delete)

--- drf_kit/models/__init__.py
+++ drf_kit/models/__init__.py
@@ -1,9 +1,38 @@
 from drf_kit.models.diff_models import ModelDiffMixin
 from drf_kit.models.file_models import BoundedFileMixin
 from drf_kit.models.base_models import BaseModel
-from drf_kit.models.soft_delete_models import SoftDeleteModel
-from drf_kit.models.ordered_models import BaseOrderedModel
-from drf_kit.models.inheritance_models import InheritanceModel
+from drf_kit.models.soft_delete_models import SoftDeleteModel, SoftDeleteModelMixin
+from drf_kit.models.ordered_models import (  # pylint: disable=reimported
+    OrderedModel,
+    OrderedModelMixin,
+    OrderedModel as BaseOrderedModel,  # retro-compatibility
+)
+from drf_kit.models.inheritance_models import InheritanceModel, InheritanceModelMixin
+
+
+class SoftDeleteInheritanceOrderedModel(SoftDeleteModelMixin, OrderedModelMixin, InheritanceModelMixin, BaseModel):
+    class Meta(SoftDeleteModelMixin.Meta, OrderedModelMixin.Meta, InheritanceModelMixin.Meta, BaseModel.Meta):
+        abstract = True
+        indexes = SoftDeleteModelMixin.Meta.indexes + OrderedModelMixin.Meta.indexes + BaseModel.Meta.indexes
+
+
+class SoftDeleteInheritanceModel(SoftDeleteModelMixin, InheritanceModelMixin, BaseModel):
+    class Meta(SoftDeleteModelMixin.Meta, InheritanceModelMixin.Meta, BaseModel.Meta):
+        abstract = True
+        indexes = SoftDeleteModelMixin.Meta.indexes + BaseModel.Meta.indexes
+
+
+class SoftDeleteOrderedModel(SoftDeleteModelMixin, OrderedModelMixin, BaseModel):
+    class Meta(SoftDeleteModelMixin.Meta, OrderedModelMixin.Meta, BaseModel.Meta):
+        abstract = True
+        indexes = SoftDeleteModelMixin.Meta.indexes + OrderedModelMixin.Meta.indexes + BaseModel.Meta.indexes
+
+
+class InheritanceOrderedModel(OrderedModelMixin, InheritanceModelMixin, BaseModel):
+    class Meta(OrderedModelMixin.Meta, InheritanceModelMixin.Meta, BaseModel.Meta):
+        abstract = True
+        indexes = OrderedModelMixin.Meta.indexes + BaseModel.Meta.indexes
+
 
 __all__ = (
     "ModelDiffMixin",
@@ -11,5 +40,10 @@ __all__ = (
     "BaseModel",
     "SoftDeleteModel",
     "BaseOrderedModel",
+    "OrderedModel",
     "InheritanceModel",
+    "SoftDeleteInheritanceOrderedModel",
+    "SoftDeleteInheritanceModel",
+    "SoftDeleteOrderedModel",
+    "InheritanceOrderedModel",
 )
