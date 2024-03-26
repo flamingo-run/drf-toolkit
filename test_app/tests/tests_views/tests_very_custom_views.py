aacdaa06917ebe0fe50fad1272cb1c2c2b12856e
Joao Daher <joao@daher.dev>
2024-03-26 15:36:55 -0300

    test: update test after django upgrade

--- test_app/tests/tests_views/tests_very_custom_views.py
+++ test_app/tests/tests_views/tests_very_custom_views.py
@@ -50,7 +50,7 @@ class TestVeryCustomView(HogwartsTestMixin, BaseApiTest):
             "id": ANY,
             "name": "Luna Lovegood",
             "age": None,
-            "is_half_blood": True,
+            "is_half_blood": False,
             "received_letter_at": None,
             "created_at": ANY,
             "updated_at": ANY,
