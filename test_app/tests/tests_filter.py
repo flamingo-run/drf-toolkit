49863557ab197367999e04080975dd57466f5582
lucasgomide <lucaslg200@gmail.com>
2024-06-21 10:33:50 -0300

    feat: support a initial (default) filter's value be a callable
    Previosly the initial filter value only accept literal value, after this change it will accept a callable to process complex operations

--- test_app/tests/tests_filter.py
+++ test_app/tests/tests_filter.py
@@ -1,5 +1,6 @@
 from drf_kit import filters
 from drf_kit.tests import BaseApiTest
+from test_app.tests.factories.beast_factories import BeastFactory
 from test_app.tests.factories.teacher_factories import TeacherFactory
 from test_app.tests.tests_base import HogwartsTestMixin
 
@@ -246,3 +247,38 @@ class TestAllOfFilter(BaseApiTest):
     def test_raise_exception_when_created_alloffilter_with_conjoined_false(self):
         with self.assertRaisesMessage(ValueError, "AllOfFilter must be conjoined=True"):
             filters.AllOfFilter(conjoined=False)
+
+
+class TestCallableInitialFilter(BaseApiTest):
+    url = "/beasts"
+
+    def test_filter_with_initial_values(self):
+        beasts = [
+            BeastFactory(is_active=True),
+        ]
+
+        BeastFactory(is_active=False)  # noise
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(200, response.status_code)
+
+        results = response.json()["results"]
+        expected_ids = [b.pk for b in beasts]
+        result_ids = [r["id"] for r in results]
+        self.assertEqual(expected_ids, list(reversed(result_ids)))
+
+    def test_filter_overwrite_initial_filters(self):
+        beasts = [BeastFactory(is_active=False)]
+
+        # noise
+        BeastFactory(is_active=True)
+        BeastFactory(is_active=True)
+        response = self.client.get(self.url, data={"is_active": 0})
+
+        self.assertEqual(200, response.status_code)
+
+        results = response.json()["results"]
+        expected_ids = [b.pk for b in beasts]
+        result_ids = [r["id"] for r in results]
+        self.assertEqual(expected_ids, list(reversed(result_ids)))
