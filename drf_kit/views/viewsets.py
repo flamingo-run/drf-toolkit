ec3c87a228e874e013a0ecd00d03a5d356c34bcd
Otto Fernandes <ottofernandes2012@gmail.com>
2024-07-02 20:37:52 -0300

    feat: refactor /search
    Having a view method calling another adds complexity to the viewset. For example, it is harder to handle cache if two different methods have their own cache rules. This change aims to make it easier for upcoming changes to deal with view methods gracefully.

--- drf_kit/views/viewsets.py
+++ drf_kit/views/viewsets.py
@@ -156,7 +156,15 @@ class SearchMixin:
         filter_backends=[filters.FilterInBodyBackend, *api_settings.DEFAULT_FILTER_BACKENDS],
     )
     def search(self, request):
-        return self.list(request)
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class ModelViewSet(MultiSerializerMixin, viewsets.ModelViewSet):
